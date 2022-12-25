e235d0b1ef85450c6238a6a3982963b44c122625
Knot <akthakorn@gmail.com>
2022-12-25 19:08:08 +0700

    setup to deploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    // appDir: true,
+    appDir: false,
   },
-}
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
